896be4279f4abfb8316bbfa26d305a27074f763b
alkado_hs <alkadosichone@gmail.com>
2023-05-24 14:07:53 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Box>
+      <Button colorScheme="blue">My Button</Button>
+    </Box>
+  );
+}
+
+export default App;
