a3f1a45b3915f7f6c15fa7c4dc07fd1c5687c4f5
alkado_hs <alkadosichone@gmail.com>
2023-05-25 22:29:29 +0300

    optmizing images

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,6 @@ import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
-
   return (
     <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10}>
       {error && <Text color={"red"}>{error}</Text>}
